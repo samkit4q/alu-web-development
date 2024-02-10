f57ac86bb79db0672de91eaded63e1fd6ae228d3
samkit4q <a.aderoba@alustudent.com>
2024-02-10 04:57:43 -0800

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const file = process.argv[2];
-fs.readFile(file, 'utf-8', function (err, data) {
+const FileName = process.argv[2];
+fs.readFile(FileName, 'utf8', function (err, line) {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    console.log(line);
   }
 });
--- javascript-web_scraping/README.md
+++ javascript-web_scraping/README.md
@@ -1,1 +1,2 @@
-sam is cool 
+am is good 
+
